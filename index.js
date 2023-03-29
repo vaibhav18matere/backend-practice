a9a34127a73e2c80f66dabdebac183071f4c4d3e
Vaibhav Matere <vpm00718897@gmail.com>
2023-03-29 19:05:12 +0530

    open file from express

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import express from "express";
+import path from "path";
 
 const app = express(); // server created
 
 app.listen(5000, () => {
   console.log("server on port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello there");
+  // res.sendStatus(307);
+});
+
+app.get("/user", (req, res) => {
+  res.json({
+    name: "vaibhav",
+    age: 25,
+  });
+});
+
+app.get("/blog", (req, res) => {
+  const pathLocation = path.resolve();
+  // console.log(path.join(pathLocation, "./index.html"));
+  res.sendFile(path.join(pathLocation, "./index.html"));
 });
