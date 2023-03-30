ab0030c1a49204f9dac954e749f50c0b8e8dc2dd
Vaibhav Matere <vpm00718897@gmail.com>
2023-03-30 15:09:13 +0530

    ejs file variable name acess methods

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ import path from "path";
 
 const app = express(); // server created
 
-app.listen(5000, () => {
-  console.log("server on port 5000");
-});
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("hello there");
-  // res.sendStatus(307);
+  res.render("index", { name: "VAIBHAV", age: 25 });
 });
 
-app.get("/user", (req, res) => {
-  res.json({
-    name: "vaibhav",
-    age: 25,
-  });
-});
+// app.get("/user", (req, res) => {
+//   res.json({
+//     name: "vaibhav",
+//     age: 25,
+//   });
+// });
 
-app.get("/blog", (req, res) => {
-  const pathLocation = path.resolve();
-  // console.log(path.join(pathLocation, "./index.html"));
-  res.sendFile(path.join(pathLocation, "./index.html"));
+// app.get("/blog", (req, res) => {
+//   const pathLocation = path.resolve();
+//   // console.log(path.join(pathLocation, "./index.html"));
+//   res.sendFile(path.join(pathLocation, "./index.html"));
+// });
+
+app.listen(5000, () => {
+  console.log("server on port 5000");
 });
