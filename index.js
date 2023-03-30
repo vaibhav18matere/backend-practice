bc7d09da9f4c3fb3a24e47280d2b5681d49a3f4c
Vaibhav Matere <vpm00718897@gmail.com>
2023-03-30 18:49:50 +0530

    get users data on separate file

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ app.post("/", (req, res) => {
   console.log(tempUsers);
 });
 
+// get users data on separate file
+app.get("/users", (req, res) => {
+  res.json({
+    tempUsers,
+  });
+});
+
+app.post("/contact", (req, res) => {
+  tempUsers.push({ userName: req.body.name, userEmail: req.body.email });
+  res.redirect("/success");
+});
+
 // app.get("/user", (req, res) => {
 //   res.json({
 //     name: "vaibhav",
