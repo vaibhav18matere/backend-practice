508ef618ef917dbaeb0e61f6998a40d582e3e736
Vaibhav Matere <vpm00718897@gmail.com>
2023-04-04 17:25:38 +0530

    login, log-out using jwt

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 const app = express(); // server created
 
 app.set("view engine", "ejs");
-
+app.use(express.json());
 app.use(cookieParser());
 
-const isAuthenticatedCheck = (req, res, next) => {
+const isAuthenticatedCheck = async (req, res, next) => {
+  console.log(req.user);
+
   const { token } = req.cookies;
   if (token) {
+    const decoded = jwt.verify(token, "jwtTimePassSecreatTempBanawalaAahe");
+    console.log(decoded);
+
+    req.user = await User.findById(decoded._id);
+
     next();
   } else {
     res.render("login");
@@ -18,14 +26,29 @@ const isAuthenticatedCheck = (req, res, next) => {
 };
 
 app.get("/", isAuthenticatedCheck, (req, res) => {
-  res.render("logout");
+  res.render("logout", { name: req.user.name });
 });
 
-app.post("/login", (req, res) => {
-  res.cookie("token", "cookieIsThere", {
-    httpOnly: true,
-    // expires: new Date(Date.now() + 40 * 1000),
+app.post("/login", async (req, res) => {
+  const { name, email } = req.body;
+  console.log(name, email);
+  const user = await User.create({
+    name,
+    email,
   });
+
+  const token = jwt.sign(
+    {
+      _id: user._id,
+    },
+    "jwtTimePassSecreatTempBanawalaAahe"
+  );
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 40 * 1000),
+  });
+
   res.redirect("/");
 });
 
@@ -36,39 +59,8 @@ app.get("/logout", (req, res) => {
   });
   res.redirect("/");
 });
-
-// make route for success
-app.get("/success", (req, res) => {
-  res.render("success");
-});
-
-// to access static path / folder, we use app.use() and add middleware
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
-
-app.post("/", (req, res) => {
-  // console.log(req.body);
-  tempUsers.push({ userName: req.body.name, userEmail: req.body.email });
-  // res.render("success");
-  //to make a simple redirect
-  res.redirect("/success");
-  console.log(tempUsers);
-});
-
-// get users data on separate file
-app.get("/users", (req, res) => {
-  res.json({
-    tempUsers,
-  });
-});
-
-app.post("/contact", async (req, res) => {
-  const { name, email } = req.body;
-
-  await userInfo.create({ name, email });
-  res.redirect("/success");
-});
-
 mongoose
   .connect("mongodb://localhost:27017", {
     dbName: "formDB",
@@ -76,22 +68,12 @@ mongoose
   .then(() => console.log("connectiong with db successful!!!"))
   .catch(() => console.log("db not connected"));
 
-const messageSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: String,
   email: String,
 });
 
-const userInfo = mongoose.model("Users", messageSchema);
-
-app.get("/add", async (req, res) => {
-  await userInfo.create({
-    name: "Test Credentials",
-    email: "test123@gmail.com",
-  });
-  res.send("connected mongodb - now schema is created");
-  // go to "/add" url and check compass data is there after reloading
-});
-
+const User = mongoose.model("User", userSchema);
 app.listen(5000, () => {
   console.log("server on port 5000");
 });
